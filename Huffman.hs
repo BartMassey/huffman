46a470cde0b7cf0d13a94c74fd6bfdc1b996bec5
Bart Massey <bart@cs.pdx.edu>
2008-05-09 06:15:04 +0000

    finished huffman tree structure

--- Huffman.hs
+++ Huffman.hs
@@ -2,11 +2,25 @@ module Huffman
 where
 
 import Data.Maybe
-import Data.Map as Map
+import qualified Data.Map as Map
+import Data.Heap
 
-freq :: (Ord a, Integral b) => [a] -> [(a, b)]
-freq l = Map.toList tab where
+data (Integral a) =>
+    HTree a b = HNode { count :: a,
+                        left :: HTree a b,
+                        right :: HTree a b }
+              | HLeaf { count :: a,
+                        label :: b }
+    deriving Eq
+
+freq :: (Integral a, Ord b) => [b] -> [HTree a b]
+freq l = map make_hleaf (Map.toList tab) where
     tab = foldl accum Map.empty l
     accum m k = Map.alter incr k m
     incr Nothing = Just 1
     incr (Just x) = Just (x + 1)
+    make_hleaf (l, n) = HLeaf { count = n, label = l }
+
+instance (Integral a, Eq b) => Ord (HTree a b) where
+    t1 `compare` t2 = count t1 `compare` count t2
+
