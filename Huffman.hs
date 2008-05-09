5c6defbcb4f3ad3df4ef1c5d8f6d7a1d8d9613fd
Bart Massey <bart@cs.pdx.edu>
2008-05-09 08:55:53 +0000

    added license

--- Huffman.hs
+++ Huffman.hs
@@ -1,3 +1,8 @@
+--- Copyright © 2008 Bart Massey
+--- ALL RIGHTS RESERVED
+--- [This program is licensed under the "3-clause ('new') BSD License"]
+--- Please see the end of this file for license terms.
+
 module Huffman (HTree, makeHTree, HTable, makeHTable, encode, decode)
 where
 
@@ -67,3 +72,33 @@ decode h = decode' h where
     decode' (HNode left _) (False : l) = decode' left l
     decode' (HNode _ right) (True : l) = decode' right l
                                              
+
+--- Redistribution and use in source and binary forms, with or
+--- without modification, are permitted provided that the
+--- following conditions are met:
+---     * Redistributions of source code must retain the above
+---       copyright notice, this list of conditions and the following
+---       disclaimer.
+---     * Redistributions in binary form must reproduce the
+---       above copyright notice, this list of conditions and the
+---       following disclaimer in the documentation and/or other
+---       materials provided with the distribution.
+---     * Neither the name of the copyright holder, nor the names
+---       of other affiliated organizations, nor the names
+---       of other contributors may be used to endorse or promote
+---       products derived from this software without specific prior
+---       written permission.
+--- 
+--- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+--- CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+--- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+--- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+--- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+--- CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+--- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+--- NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+--- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+--- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+--- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+--- OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+--- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
