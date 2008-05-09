fd286bdedaad7729b598630763aff632f71b58e5
Bart Massey <bart@cs.pdx.edu>
2008-05-09 18:44:32 +0000

    finished test; still stack overflow bug

--- huffmantest.hs
+++ huffmantest.hs
@@ -1,3 +1,4 @@
+import System.Environment
 import Huffman
 
 -- ruler function
@@ -10,3 +11,11 @@ huffman_data :: Int -> [Int]
 huffman_data n = take n (ruler i) where
     i = ceiling (1 + (logBase 2 (fromIntegral n)))
 
+roundtrip l = decode tree (encode table l) where
+    tree = makeHTree HInitNone l
+    table = makeHTable tree
+
+main = do
+  [arg] <- getArgs
+  let h = huffman_data (read arg)
+  (putStrLn . show . (== h) . roundtrip) h
