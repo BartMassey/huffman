8d123711362242d6ec95824662ee73089c59a539
Bart Massey <bart@cs.pdx.edu>
2008-05-09 06:35:48 +0000

    finished tree builder; no testing yet

--- Huffman.hs
+++ Huffman.hs
@@ -1,9 +1,10 @@
 module Huffman
 where
 
+import Data.List
 import Data.Maybe
 import qualified Data.Map as Map
-import Data.Heap
+import qualified Data.Heap as Heap
 
 data (Integral a) =>
     HTree a b = HNode { count :: a,
@@ -15,7 +16,7 @@ data (Integral a) =>
 
 freq :: (Integral a, Ord b) => [b] -> [HTree a b]
 freq l = map make_hleaf (Map.toList tab) where
-    tab = foldl accum Map.empty l
+    tab = foldl' accum Map.empty l
     accum m k = Map.alter incr k m
     incr Nothing = Just 1
     incr (Just x) = Just (x + 1)
@@ -24,3 +25,19 @@ freq l = map make_hleaf (Map.toList tab) where
 instance (Integral a, Eq b) => Ord (HTree a b) where
     t1 `compare` t2 = count t1 `compare` count t2
 
+makeHTree :: (Integral a, Ord b) => [b] -> HTree a b
+makeHTree = treeify . from_list . freq where
+    from_list :: (Integral a, Ord b) =>
+                 [HTree a b] -> Heap.MinHeap (HTree a b)
+    from_list = Heap.fromList
+    treeify h = treeify1 v' h' where
+        (v', h') = Heap.extractHead h
+    treeify1 v h
+        | Heap.isEmpty h = v
+        | otherwise = treeify2 v v' h' where
+            (v', h') = Heap.extractHead h
+    treeify2 v1 v2 h = treeify h' where
+        h' = Heap.insert v h
+        v = HNode { count = count v1 + count v2,
+                    left = v1,
+                    right = v2 }
