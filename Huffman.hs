c5f7fe4f8a10723c1c5cffea3c57ff9e506d82f0
Bart Massey <bart@cs.pdx.edu>
2008-05-09 07:35:43 +0000

    exposed abstract htree for module purposes

--- Huffman.hs
+++ Huffman.hs
@@ -1,4 +1,4 @@
-module Huffman (HTree, makeHTree)
+module Huffman (HTree(..), makeHTree)
 where
 
 import Data.List
@@ -7,37 +7,40 @@ import qualified Data.Map as Map
 import qualified Data.Heap as Heap
 
 data (Integral a) =>
-    HTree a b = HNode { count :: a,
-                        left :: HTree a b,
-                        right :: HTree a b }
-              | HLeaf { count :: a,
-                        label :: b }
+    HT a b = HN { count :: a,
+                  left :: HT a b,
+                  right :: HT a b }
+           | HL { count :: a,
+                  label :: b }
     deriving Eq
 
-instance (Integral a, Show a, Show b) => Show (HTree a b) where
+instance (Integral a, Show a, Show b) => Show (HT a b) where
     show h = show' 0 h where
-        show' i (HLeaf {count = c, label = l}) =
+        show' i (HL {count = c, label = l}) =
             replicate i ' ' ++ "<#" ++ show c ++ " " ++ show l ++ ">"
-        show' i (HNode {count = c, left = l, right = r}) =
+        show' i (HN {count = c, left = l, right = r}) =
             replicate i ' ' ++ "<#" ++ show c ++ "\n" ++
             show' (i + 2) l ++ "\n" ++
             show' (i + 2) r ++ ">"
 
-freq :: (Integral a, Ord b) => [b] -> [HTree a b]
+data HTree a = HNode (HTree a) (HTree a)
+             | HLeaf a
+
+freq :: (Integral a, Ord b) => [b] -> [HT a b]
 freq l = map make_hleaf (Map.toList tab) where
     tab = foldl' accum Map.empty l
     accum m k = Map.alter incr k m
     incr Nothing = Just 1
     incr (Just x) = Just (x + 1)
-    make_hleaf (l, n) = HLeaf { count = n, label = l }
+    make_hleaf (l, n) = HL { count = n, label = l }
 
-instance (Integral a, Eq b) => Ord (HTree a b) where
+instance (Integral a, Eq b) => Ord (HT a b) where
     t1 `compare` t2 = count t1 `compare` count t2
 
-makeHTree :: (Integral a, Ord b) => [b] -> HTree a b
-makeHTree = treeify . from_list . freq where
+makeHTree :: (Ord b) => [b] -> HTree b
+makeHTree = ht_extract . treeify . from_list . freq where
     from_list :: (Integral a, Ord b) =>
-                 [HTree a b] -> Heap.MinHeap (HTree a b)
+                 [HT a b] -> Heap.MinHeap (HT a b)
     from_list = Heap.fromList
     treeify h = treeify1 v' h' where
         (v', h') = Heap.extractHead h
@@ -47,6 +50,16 @@ makeHTree = treeify . from_list . freq where
             (v', h') = Heap.extractHead h
     treeify2 v1 v2 h = treeify h' where
         h' = Heap.insert v h
-        v = HNode { count = count v1 + count v2,
-                    left = v1,
-                    right = v2 }
+        v = HN { count = count v1 + count v2,
+                 left = v1,
+                 right = v2 }
+    ht_extract :: (Integral a) => HT a b -> HTree b
+    ht_extract (HN {left = l, right = r}) =
+        HNode (ht_extract l) (ht_extract r)
+    ht_extract (HL {label = l}) =
+        HLeaf l
+
+-- encode :: (Ord b) => HTable a b -> [b] -> [Bool]
+-- encode _ [] = []
+-- encode h (e : es) = treewalk ++ encode h es where
+
