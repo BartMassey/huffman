7a4f160700232095c2ea02ce991151aa91ff2dd1
Bart Massey <bart@cs.pdx.edu>
2008-05-10 05:27:29 +0000

    applied license to test code

--- huffmantest.hs
+++ huffmantest.hs
@@ -1,3 +1,9 @@
+--- Copyright © 2008 Bart Massey
+--- ALL RIGHTS RESERVED
+--- [This program is licensed under the "3-clause ('new') BSD License"]
+--- Please see the file COPYING in the source
+--- distribution of this software for license terms.
+
 import System.Environment
 import Huffman
 
