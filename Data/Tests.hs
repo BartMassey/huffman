1aa27fbf3ecc720aea633c14ae2a160fc1931a79
Bart Massey <bart@cs.pdx.edu>
2008-07-29 21:39:58 +0000

    added decode / encode quickcheck test; still fails

--- Data/Tests.hs
+++ Data/Tests.hs
@@ -12,7 +12,14 @@ instance Arbitrary Char where
     arbitrary = sized $ \n -> frequency
         [(n `div` i, return $ chr (ord 'a' + i)) | i <- [1 .. n `min` 26]]
 
+prop_encode_decode l = (decode tree . encode table $ l) == id l where
+    l' = sort . freq [] $ l :: [Freq Int Char]
+    tree = makeHTree l'
+    table = makeHTable tree
+
 prop_make_recon l = (reconstructHTree . makeHTable $ t) == id t where
     t = makeHTree . sort . freq [] $ l :: HTree Char
 
-tests = [("reconstructHTree.makeHTable/id", test prop_make_recon)]
+tests = [("decode.encode/id", test prop_encode_decode),
+         ("reconstructHTree.makeHTable/id", test prop_make_recon)]
+         
